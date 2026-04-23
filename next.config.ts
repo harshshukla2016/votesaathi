5b6d94c22aadd61d34ec69d56f833801d0671c1c
Harsh Shukla <harshshukla2017@gmail.com>
2026-04-23 05:33:17 +0530

    Absolute Excellence: Production Build Ready (Vercel Hardened)

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
         pathname: "/**",
       },
     ],
